a8b84b79ad8d3fdb02d879065ebd80d1708b5fde
Andrew Bell <fiixed@gmail.com>
2018-12-04 15:49:43 -0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:weight_on_planet_x/ui/home.dart';
+
+void main() => runApp(Home());
+
---
+++ lib/ui/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+
+}
+
+class HomeState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+
+}
