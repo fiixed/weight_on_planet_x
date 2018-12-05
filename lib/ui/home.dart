02bd5b8a4a4c0e4df72fb3397b4d0e31a01f56ea
Andrew Bell <fiixed@gmail.com>
2018-12-05 08:47:05 -0600

    finished logic

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -8,12 +8,34 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State {
+  final TextEditingController _weightController = TextEditingController();
   int radioValue = 0;
+  double _finalResult = 0.0;
+  String _formattedText = '';
 
   void handleRadioValueChanged(int value) {
     setState(() {
       radioValue = value;
-      print(radioValue);
+      switch (radioValue) {
+        case 0:
+          _finalResult = calculateWeight(_weightController.text, 0.06);
+          _formattedText =
+              'Your weight on Pluto is ${_finalResult.toStringAsFixed(1)}';
+          break;
+        case 1:
+          _finalResult = calculateWeight(_weightController.text, 0.38);
+          _formattedText =
+              'Your weight on Mars is ${_finalResult.toStringAsFixed(1)}';
+
+          break;
+        case 2:
+          _finalResult = calculateWeight(_weightController.text, 0.91);
+          _formattedText =
+              'Your weight on Venus is ${_finalResult.toStringAsFixed(1)}';
+
+          break;
+        default:
+      }
     });
   }
 
@@ -42,7 +64,7 @@ class HomeState extends State {
                 child: Column(
                   children: <Widget>[
                     TextField(
-                      controller: null,
+                      controller: _weightController,
                       keyboardType: TextInputType.number,
                       decoration: InputDecoration(
                         labelText: 'Your Weight on Earth',
@@ -85,7 +107,9 @@ class HomeState extends State {
                     ),
                     Padding(padding: EdgeInsets.all(15.6)),
                     Text(
-                      'Hello There',
+                      _weightController.text.isEmpty
+                          ? "Please enter weight"
+                          : _formattedText + " lbs",
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: 19.4,
@@ -99,5 +123,14 @@ class HomeState extends State {
         ),
       ),
     );
+  }
+
+  double calculateWeight(String weight, double multiplier) {
+    if (int.parse(weight).toString().isNotEmpty && int.parse(weight) > 0) {
+      return int.parse(weight) * multiplier;
+    } else {
+      print('Wrong!');
+      return int.parse('180') * 0.38;
+    }
   }
 }
